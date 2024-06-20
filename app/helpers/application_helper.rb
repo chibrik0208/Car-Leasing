6234ac0bc02d413f6e734a6bf4b02fe0f777baa4
chibrik0208 <ilya.ismailau@gmail.com>
2024-06-20 23:32:36 +0200

    added helper and show view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def join_and_clean(collection, separator = ', ')
+    collection = [collection] if collection.is_a?(String)
+
+    cleaned_collection = collection.reject(&:blank?) if collection.respond_to?(:reject)
+
+    if cleaned_collection.nil? || cleaned_collection.empty?
+      "None"
+    else
+      cleaned_collection.join(separator)
+    end
+  end
 end
