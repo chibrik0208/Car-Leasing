e4306e7dd3f70e7337d4db78743bf1c8332c67ee
chibrik0208 <ilya.ismailau@gmail.com>
2024-06-26 22:25:54 +0200

    added routes for sorted car pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
-  resources :cars
+  resources :cars do
+    collection do
+      get 'page/:page', action: :index, as: 'page'
+    end
+  end
 
-  get 'new_cars', to: 'cars#index'
+  get '/new_cars', to: 'cars#index', category: 'NEW'
+  get '/pre-owned_cars', to: 'cars#index', category: 'PRE_OWNED'
+  get '/cars/:category/:body_type', to: 'cars#index', as: 'filtered_cars'
+
   get 'contact', to: 'pages#contact'
 end
